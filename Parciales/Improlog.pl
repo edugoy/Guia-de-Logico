8340e946236883c74247275aee0fe519cf2fe418
Eduardo Goytendia <egoytendia@dialdata.com.ar>
2025-09-08 17:34:51 -0300

    terminamos el parcial de Improlog

--- Parciales/Improlog.pl
+++ Parciales/Improlog.pl
@@ -49,3 +49,71 @@ seDestaca(Grupo, Integrante) :-
 grupo(vientosDelEste, bigBand).
 grupo(sophieTrio, formacionParticular([contrabajo, guitarra, violin])).
 grupo(jazzmin, formacionParticular([bateria, bajo, trompeta, piano, guitarra])).
+
+hayCupo(Grupo, Instrumento) :-
+    grupo(Grupo, bigBand),
+    instrumento(Instrumento, melodico(viento)).
+
+hayCupo(Grupo, Instrumento) :-
+    grupo(Grupo, formacionParticular(ListaInstrumentos)),
+    member(Instrumento, ListaInstrumentos),
+    not(integrante(Grupo, _, Instrumento)).
+
+sirveBigBand(bateria).
+sirveBigBand(bajo).
+sirveBigBand(piano).
+
+hayCupo(Grupo, Instrumento) :-
+    grupo(Grupo, bigBand),
+    sirveBigBand(Instrumento),
+    not(integrante(Grupo, _, Instrumento)).
+
+puedeIncorporarse(Grupo, Integrante, Instrumento) :-
+    integrante(_, Integrante, Instrumento),
+    not(integrante(Grupo, Integrante, _)),
+    hayCupo(Grupo, Instrumento),
+    nivelQueTiene(Integrante, Instrumento, Nivel),
+    requerimiento(Grupo, Nmin),
+    Nivel >= Min.
+
+requerimiento(Grupo, Min) :-
+    grupo(Grupo, bigBand),
+    Min is 1.
+
+requerimiento(Grupo, Min) :-
+    grupo(Grupo, formacionParticular(ListaInstrumentos)),
+    length(ListaInstrumentos, CantidadInstrumentos),
+    Min is 7 - CantidadInstrumentos.
+
+seQuedoEnBanda(Integrante) :-
+    not(integrante(_, Integrante, _)),
+    not(puedeIncorporarse(_, Integrante, _)).
+
+puedeTocar(Grupo) :-
+    grupo(Grupo, bigBand),
+    tieneUnaBuenaBase(Grupo),
+    findall(Integrante, tocaTipoViento(Grupo, Integrante), Integrantes),
+    length(Integrantes, Cantidad),
+    Cantidad >= 5.
+
+tocaTipoViento(Grupo, Integrante) :-
+    integrante(Grupo, Integrante, Instrumento),
+    instrumento(Instrumento, melodico(viento)).
+
+puedeTocar(Grupo) :-
+    grupo(Grupo, formacionParticular(ListaInstrumentos)),
+    forall(member(Instrumento, ListaInstrumentos), integrante(Grupo, _, Instrumento)).
+
+grupo(studio1, ensamble(3)).
+
+requerimiento(Grupo, Min) :-
+    grupo(Grupo, ensamble(Min)).
+
+sirveParaGrupo(Grupo, _) :-
+    grupo(Grupo, ensamble(_)).
+
+puedeTocar(Grupo) :-
+    grupo(Grupo, ensamble(_)),
+    tieneUnaBuenaBase(Grupo),
+    integrante(Grupo, _, Instrumento),
+    instrumento(Inst, melodico(_)).
